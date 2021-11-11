6182c5bd2dc2cd9665a68eb6e5bca4d7ceb82d42
MPavelDeveloper <90277675+MPavelDeveloper@users.noreply.github.com>
2021-11-11 17:55:02 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  url: `http://scw000112219:8081/api/genealogy`
+    url: `http://scw000112219:8081/api/genealogy`,
+  login: `http://scw000112219:8081/api`,
+  registry: `http://scw000112219:8081/api`,
 };
