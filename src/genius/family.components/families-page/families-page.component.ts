2af73cae59ef57e1d8c389a99cf0eb56a3f2ec3b
MPavelDeveloper <pashokkk3@mail.ru>
2021-11-25 15:16:18 +0300

    Task #No 16: 1. Optimize render family tree

--- src/genius/family.components/families-page/families-page.component.ts
+++ src/genius/family.components/families-page/families-page.component.ts
@@ -12,22 +12,27 @@ import {FamilyTreeService, Node} from '../../services/family-tree/family-tree.se
   styleUrls: ['./families-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FamiliesPageComponent implements OnInit, OnDestroy{
-
+export class FamiliesPageComponent implements OnInit, OnDestroy {
   private dataLoadSubscription: Subscription;
   public families: Array<Family>;
   public familyId: number;
   public confirmDialogVisible: boolean;
-  public familyGenius: Array<Array<Node>>;
-  public familyTreeBootstrap: Array<Array<Array<Node>>>;
+  public familyTreeGrid: Array<Array<Array<Node>>>;
+  private dataProvider;
+  private dataLoad;
+  private familyTree;
+  private changeDetection;
 
-  constructor(private dataProvider: DataProvider, private dataLoad: DataLoadService, private familyTree: FamilyTreeService, private changeDetection: ChangeDetectorRef) {
+  constructor(dataProvider: DataProvider, dataLoad: DataLoadService, familyTree: FamilyTreeService, changeDetection: ChangeDetectorRef) {
+    this.dataProvider = dataProvider;
+    this.dataLoad = dataLoad;
+    this.familyTree = familyTree;
+    this.changeDetection = changeDetection;
     this.loadFamilyTree();
   }
 
   ngOnInit() {
     this.dataLoadSubscription = this.dataLoad.families$.subscribe(() => {
-      this.familyGenius = [];
       this.loadFamilyTree();
     });
   }
@@ -56,7 +61,8 @@ export class FamiliesPageComponent implements OnInit, OnDestroy{
 
   public loadFamilyTree(): void {
     this.familyTree.createFamilyTree().subscribe(() => {
-        this.familyTreeBootstrap = this.createFamilyBootstrap(this.familyTree.getFamilyTreeLevels());
+        this.familyTreeGrid = this.createFamilyTreeGrid(this.familyTree.getFamilyTreeLevels());
+        console.log(this.familyTreeGrid);
         this.changeDetection.detectChanges();
       },
       (errorResponse) => {
@@ -64,96 +70,111 @@ export class FamiliesPageComponent implements OnInit, OnDestroy{
       });
   }
 
-  public createFamilyBootstrap(familyTreeLevels: Array<Array<Node>>): Array<Array<Array<Node>>> {
-    let familyTreeBootstrap: Array<Array<Array<Node>>> = [];
-    let nodeAmountParentLevel = 0;
-    let nodeAmountChildCurrentLevel = 0;
-    let gridCounter = 0;
+  public createFamilyTreeGrid(familyTreeLevels: Array<Array<Node>>): Array<Array<Array<Node>>> {
+    let familyTreeGrid: Array<Array<Array<Node>>> = [];
+    let rootFamilyNode: Array<Array<Node>> = [familyTreeLevels[0]]
+    let nodeChildMaxNumber = 0;
 
-    // push #root
-    // VVV
-    familyTreeBootstrap.push([familyTreeLevels[0]]);
-    for (let currentRowIndex = 0; currentRowIndex < familyTreeBootstrap.length; currentRowIndex++) {
-      nodeAmountChildCurrentLevel = 0;
-
-      // [[node], [node]] nodeSets
-      nodeAmountParentLevel = familyTreeBootstrap[currentRowIndex].length;
-
-
-      let parentNodesAmount = 0;
-      let childNodesAmount = 0;
-
-      familyTreeBootstrap[currentRowIndex].forEach((nodes: Array<Node>) => {
-        parentNodesAmount ++;
-        nodes.forEach(node => {
-          if(node !== null) {
-            let currentChildAmount = node.children.length;
-            if(currentChildAmount > childNodesAmount) {
-              childNodesAmount = currentChildAmount;
-            }
-          }
-        });
-      });
-
-
-      if (parentNodesAmount > nodeAmountParentLevel) {
-        nodeAmountParentLevel = parentNodesAmount;
-      }
-
-      if (childNodesAmount > nodeAmountChildCurrentLevel) {
-        nodeAmountChildCurrentLevel = childNodesAmount
-      }
-      childNodesAmount = 0;
-      // console.log('parents: ', parentNodesAmount);
-      // console.log('chidren: ', childNodesAmount);
-
-      if (nodeAmountParentLevel > nodeAmountChildCurrentLevel) {
-        gridCounter = nodeAmountParentLevel
+    familyTreeGrid.push(rootFamilyNode);
+    for (let currentFamilyNodeSet of familyTreeGrid) {
+      let currentRowNodes = this.getCurrentRowNodes(currentFamilyNodeSet);
+      nodeChildMaxNumber = this.getChildNodeMaxNumber(currentRowNodes);
+      let nextFamilyGridRow = this.createNewFamilyTreeGridRow(currentRowNodes, nodeChildMaxNumber);
+      if (this.checkNextFamilyGridRow(nextFamilyGridRow)) {
+        familyTreeGrid.push(nextFamilyGridRow);
       } else {
-        gridCounter = nodeAmountChildCurrentLevel;
+        break
       }
-
-      // console.log('gridCounter: ', gridCounter)
-
-      let currentLevelNodes: Array<Node> = [];
-
-      // [[node], [node]]
-      familyTreeBootstrap[currentRowIndex].forEach((nodes: Array<Node>) => currentLevelNodes = currentLevelNodes.concat(nodes));
-      // console.log(currentLevelNodes)
-
-
-      familyTreeBootstrap.push(this.createNewFamilyTreeBootstrapLevel(currentLevelNodes, gridCounter));
-      if(currentRowIndex === familyTreeLevels.length) break
     }
-    return familyTreeBootstrap;
+    return familyTreeGrid;
   }
 
-  public createNewFamilyTreeBootstrapLevel(currentLevelNodes: Array<Node>, maxChildrenNodes: number): Array<Array<Node>> {
-    let familyTreeBootstrapRow: Array<Array<Node>> = [];
-    currentLevelNodes.forEach(node => {
-
-
-      if (node !== null) {
-        let nodeChildren: Array<Node> = [].concat(node.children);
-        while (nodeChildren.length < maxChildrenNodes) {
-          nodeChildren.push(null);
+  private getChildNodeMaxNumber(nodes: Array<Node>): number {
+    let maxChildNodeNumber = 0;
+    // [[node, node], [node]]
+    nodes.forEach(node => {
+      if (node.data !== null) {
+        if (node.children.length > maxChildNodeNumber) {
+          maxChildNodeNumber = node.children.length;
         }
-        familyTreeBootstrapRow.push(nodeChildren);
-      } else {
-        let nodeChildren: Array<Node> = []
-        while (nodeChildren.length < maxChildrenNodes) {
-          nodeChildren.push(null);
-        }
-        familyTreeBootstrapRow.push([null]);
       }
-
     });
-
-    return familyTreeBootstrapRow;
+    return maxChildNodeNumber;
   }
 
-  public compareIndexAndOrder(index: number, order: number): boolean {
-    return index === order;
+  private getCurrentRowNodes(currentFamilyNodeSet: Array<Array<Node>>): Array<Node> {
+    let currentRowNodes: Array<Node> = [];
+    currentFamilyNodeSet.forEach(node => currentRowNodes = currentRowNodes.concat(node));
+    return currentRowNodes;
+  }
+
+  public createNewFamilyTreeGridRow(currentRowNodes: Array<Node>, nodeChildMaxNumber: number): Array<Array<Node>> {
+    let familyTreeGridRow: Array<Array<Node>> = [];
+    currentRowNodes.forEach(node => {
+      let childNodes: Array<Node> = [];
+      if (node.data !== null) {
+        childNodes = node.children;
+      }
+      while (childNodes.length < nodeChildMaxNumber) {
+        childNodes.push(new Node(null));
+      }
+      familyTreeGridRow.push(childNodes);
+    });
+    return familyTreeGridRow;
+  }
+
+  private checkNextFamilyGridRow(nextFamilyGridRow: Array<Array<Node>>): boolean {
+    let nodes = this.getCurrentRowNodes(nextFamilyGridRow);
+    for (let i = 0; i < nodes.length; i++) {
+      if (nodes[i].data !== null) return true
+    }
+    return false;
+  }
+
+  public setFamilyStyle(family: Node): string {
+    if (family.data) {
+      return 'family';
+    }
+    return 'family-none';
+  }
+
+  public setArrowStyle(parentAmount: number, childNumber: number, nodeIndex: number,): string {
+    if (parentAmount === 1) {
+      switch (childNumber) {
+        case 1:
+          return 'arrow-center';
+        case 2:
+          if (nodeIndex === 0) return 'arrow-big-left';
+          if (nodeIndex === 1) return 'arrow-big-right';
+      }
+    } else if (parentAmount === 2) {
+      switch (childNumber) {
+        case 1:
+          return 'arrow-center';
+        case 2:
+          if (nodeIndex === 0) return 'arrow-left_x4';
+          if (nodeIndex === 1) return 'arrow-right_x4';
+          break;
+        case 3:
+          if (nodeIndex === 0) return 'arrow-left_x4';
+          if (nodeIndex === 1) return 'arrow-center';
+          if (nodeIndex === 2) return 'arrow-right_x4';
+      }
+    }
+
+    switch (childNumber) {
+      case 1:
+        return 'arrow-center';
+      case 2:
+        if (nodeIndex === 0) return 'arrow-left_xN';
+        if (nodeIndex === 1) return 'arrow-right_xN';
+        break;
+      case 3:
+        if (nodeIndex === 0) return 'arrow-left_x4';
+        if (nodeIndex === 1) return 'arrow-center';
+        if (nodeIndex === 2) return 'arrow-right_x4';
+    }
+    return undefined
   }
 
 }
